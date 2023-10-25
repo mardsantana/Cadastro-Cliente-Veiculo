65adac3fd742bc51e17996d726927ef88301ed3d
mardsantana <mardsonsantana105@gmail.com>
2023-10-25 14:32:07 -0300

    configurando aplicação - 1

---
+++ src/main/java/cliente/veiculos/microsservicedecadastro/MicrosserviceDeCadastroApplication.java
@@ -0,0 +1,13 @@
+package cliente.veiculos.microsservicedecadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosserviceDeCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosserviceDeCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cliente/veiculos/microsservicedecadastro/MicrosserviceDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package cliente.veiculos.microsservicedecadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosserviceDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
